fa93e5e08e46e9184e0e6e33b96046451f8b775e
Jérôme Dubromelle <jerome.dubromelle@ac-amiens.fr>
2026-02-13 11:01:55 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+const celsiusInput = document.querySelector('#celsius');
+const resultDiv = document.querySelector('#result');
+
+window.convertir = () => {
+    const c = parseFloat(celsiusInput.value);
+    if (!isNaN(c)) {
+        const f = (c * 9/5) + 32;
+        resultDiv.innerText = `${c}°C est égal à ${f.toFixed(2)}°F`;
+    }
+};
